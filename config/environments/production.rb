e70721ef2ead7394208ca11ad0e305838987f471
Andre <[andre_w3@yahoo.com]>
2017-01-18 16:48:48 -0800

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
      :address => 'smtp.gmail.com',
      :port => '587',
      :authentication => :plain,
-     :user_name: ENV["GMAIL_USERNAME"],
-     :password: ENV["GMAIL_PASSWORD"]
+     :user_name => ENV["GMAIL_USERNAME"],
+     :password => ENV["GMAIL_PASSWORD"]
      :enable_starttls_auto => true
    }
 
