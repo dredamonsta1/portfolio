36b030a672b7f596f0ac1e3b468d9c2e4dfadb39
Andre <[andre_w3@yahoo.com]>
2017-01-18 17:23:32 -0800

    fixed everything

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
 
   config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.default_url_options = {host: 'https://andrewilkinson.com'}
+   config.action_mailer.default_url_options = {host: 'https://andrewilkinson.herokuapp.com'}
    config.action_mailer.delivery_method = :smtp
    ActionMailer::Base.smtp_settings = {
      :address => 'smtp.gmail.com',
